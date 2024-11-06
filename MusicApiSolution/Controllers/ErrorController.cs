afadcd990949f5e53338c206f791774b07f68f0b
jatindershub <jatindersmail@icloud.com>
2024-11-06 21:36:20 +0100

    Tilføjet errocontroller

---
+++ MusicApiSolution/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtistInfo.Api.Controllers
+{
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public ActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
