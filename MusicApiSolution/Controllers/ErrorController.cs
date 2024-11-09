e69844e2a3b784681869a3590b7976b4f70360b9
jatindershub <jatindersmail@icloud.com>
2024-11-09 10:47:44 +0100

    Review af controller + gjort error controller hiden

--- MusicApiSolution/Controllers/ErrorController.cs
+++ MusicApiSolution/Controllers/ErrorController.cs
@@ -2,9 +2,12 @@
 
 namespace ArtistInfo.Api.Controllers
 {
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class ErrorController : ControllerBase
     {
         [Route("/error")]
+        [HttpGet] // Explicitly defining the HTTP method
         public ActionResult Error()
         {
             return Problem();
